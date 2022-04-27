4043f13626c5956b4187d8d51fffa5379c9f9932
Priyanka <priyankadombale27@gmail.com>
2022-04-28 00:40:35 +0530

    Added:forget password and reset password pages has done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,16 @@ import { AppComponent } from './app.component';
 import { RegistrationComponent } from './component/registration/registration.component';
 import { LoginComponent } from './component/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
-    LoginComponent
+    LoginComponent,
+    ForgotPasswordComponent,
+    ResetpasswordComponent
   ],
   imports: [
     BrowserModule,
