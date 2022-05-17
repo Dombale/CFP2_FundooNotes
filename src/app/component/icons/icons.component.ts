add8245b8762982e565cc0abec84869157167250
Priyanka <priyankadombale27@gmail.com>
2022-05-17 13:38:15 +0530

    Added: FundooNotes

--- src/app/component/icons/icons.component.ts
+++ src/app/component/icons/icons.component.ts
@@ -1,4 +1,4 @@
-  import { Component, EventEmitter, Input, OnInit, Output, } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, } from '@angular/core';
 import { NoteService } from 'src/app/services/noteService/note.service';
 @Component({
   selector: 'app-icons',
@@ -9,40 +9,44 @@ export class IconsComponent implements OnInit {
   @Input() noteObj: any;
   @Output() updatedIconData = new EventEmitter<any>();
   constructor(private noteservice: NoteService) { }
+  colors = [{ name: 'white', Colorcode: '#d78cb9' }, { name: 'peach', Colorcode: '#f28b82' }, { name: 'pink', Colorcode: '#FFFEA9' }, { name: 'lemon', Colorcode: '#d7aefb' }, { name: 'brown', Colorcode: '#b5e39c' }, { name: 'green', Colorcode: '#e6ba81' }, { name: 'blue', Colorcode: '#cbf0f8' }]
 
   ngOnInit(): void {
   }
 
   onDelete() {
-    let req={
-      noteIdList:[this.noteObj.id],
-      isDeleted:true
+    let req = {
+      noteIdList: [this.noteObj.id],
+      isDeleted: true
     }
     this.noteservice.deleteNote(req).subscribe((response: any) => {
       console.log("Note Deleted Successfully", response);
       this.updatedIconData.emit(response);
     })
   }
-  onArchive(){
-    let req={
-      noteIdList:[this.noteObj.id],
-      isArchived:true
+  onArchive() {
+    let req = {
+      noteIdList: [this.noteObj.id],
+      isArchived: true
     }
     this.noteservice.archivedNote(req).subscribe((response: any) => {
       console.log("Note Archived Successfully", response);
       this.updatedIconData.emit(response);
     })
   }
-  changeColor(color:any){
-    console.log('color',color);
-    this.noteObj.color=color;
-    let req={
-      noteIdList:[this.noteObj.id],
-      color:color
+  changeColor(Notecolor: any) {
+    this.noteObj.color;
+    let req = {
+      color: Notecolor,
+      noteIdList: [this.noteObj.id],
+
     }
     this.noteservice.changeColors(req).subscribe((response: any) => {
-      console.log("Color has changed Successfully", response);
-      this.updatedIconData.emit(response);
+      console.log("Note color changed Successfully", response);
+      this.updatedIconData.emit(Notecolor);
     })
+  }
+  onReminder(){
+    
   }
 }
